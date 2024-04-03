42951b574fa2e4cf05f08ce6a2d720f9d6ab2ea8
pr0kuk <alex-shch@yandex.ru>
2024-04-03 19:54:22 +0300

    used presenters

--- bookkeeper/view/category_widget.py
+++ bookkeeper/view/category_widget.py
@@ -1,32 +1,33 @@
-"""
-Edit Widget
-"""
-from PySide6 import QtWidgets
-from .edit_window import EditCtgWindow
+from typing import Any, Callable
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtWidgets import QWidget, QTreeWidgetItem, QMenu, QMessageBox
+from bookkeeper.view.presenters import CategoryPresenter
+from bookkeeper.models.category import Category
+from .category_item import CategoryItem
 
-def set_data(box: QtWidgets.QComboBox, cats: list[str]) -> None:
-    for cat in cats:
-        box.addItem(cat)
+class CategoryWidget(QWidget):
+    category_changed = QtCore.Signal()
+    category_adder: Callable[[Category], None]
+    category_modifier: Callable[[Category], None]
+    category_checker: Callable[[str], bool]
+    category_deleter: Callable[[Category], None]
+    category_finder: Callable[[str], None | int]
 
-class EditWidget(QtWidgets.QWidget):
     def __init__(self) -> None:
         super().__init__()
+        self.setWindowTitle("Изменение категорий")
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(QtWidgets.QLabel("Редактирование"))
-        cat_edit_button = QtWidgets.QPushButton("Редактировать")
-        cat_box = QtWidgets.QComboBox()
-        labels = [QtWidgets.QLabel("Сумма"),QtWidgets.QLineEdit("0"), QtWidgets.QLabel(), QtWidgets.QLabel("Категория"), cat_box, cat_edit_button, QtWidgets.QLabel(),QtWidgets.QPushButton("Добавить"), QtWidgets.QLabel()]
-        glayout = QtWidgets.QGridLayout()
-        for i, label in zip(range(len(labels)), labels):
-            print(i, i//3, i%3)
-            glayout.addWidget(label, i//3, i%3)
-        gwidget = QtWidgets.QWidget()
-        gwidget.setLayout(glayout)
-        layout.addWidget(gwidget)
-        self.cat_list = ["Продукты", "Книги"]
-        set_data(cat_box, self.cat_list)
-        cat_edit_button.clicked.connect(self.open_window)
+        self.categories_widget = QtWidgets.QTreeWidget()
+        self.categories_widget.setColumnCount(1)
+        self.categories_widget.setHeaderLabel('Категории')
+        layout.addWidget(self.categories_widget)
         self.setLayout(layout)
+        self.presenter = CategoryPresenter(self)
+        self.menu = QMenu(self)
+        self.menu.addAction('Добавить').triggered.connect(self.add_category_event)
+        self.menu.addAction('Удалить').triggered.connect(self.delete_category_event)
+        self.sign = self.categories_widget.itemChanged
+        self.sign.connect(self.edit_category_event)
 
     def open_window(self):
         self.edit_ctg = EditCtgWindow(self.cat_list)
