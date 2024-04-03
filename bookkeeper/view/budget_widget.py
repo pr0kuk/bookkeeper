b123c5c97bd059f853fd568bd753f27e1bbbe0ec
pr0kuk <alex-shch@yandex.ru>
2024-04-03 18:56:45 +0300

    used presenters

--- bookkeeper/view/budget_widget.py
+++ bookkeeper/view/budget_widget.py
@@ -1,25 +1,38 @@
-"""
-Budget Widget
-"""
-from PySide6.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QHeaderView, QLabel, QVBoxLayout, QAbstractItemView
-
-def set_data(table: QTableWidget, spent: list[float], day_budget: float) -> None:
-    budget = [day_budget, day_budget * 7, day_budget * 30]
-    for i, [lost, limit] in enumerate(zip(spent, budget)):
-        table.setItem(i, 0, QTableWidgetItem(str(lost)))
-        table.setItem(i, 1, QTableWidgetItem(str(limit)))
+from typing import Any, Callable
+from .budget_item import BudgetDayItem, BudgetMonthItem, BudgetWeekItem, BudgetItem
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtWidgets import QWidget, QMessageBox
+from bookkeeper.view.presenters import BudgetPresenter
+from bookkeeper.models.budget import Budget
 
 class BudgetWidget(QWidget):
-    def __init__(self) -> None:
+    budget_getter: Callable[[], Budget]
+    budget_modifier: Callable[[Budget], None]
+    exp_getter: Callable[[], list[float]]
+
+    def __init__(self, expense_presenter: Any) -> None:
         super().__init__()
-        table = QTableWidget(3, 2)
-        table.setHorizontalHeaderLabels(["Сумма ", "Бюджет "])
-        table.setVerticalHeaderLabels(["День ", "Неделя ", "Месяц "])
+        self.expenses_table = QtWidgets.QTableWidget(3, 2)
+        self.expenses_table.setHorizontalHeaderLabels(["Сумма ", "Бюджет "])
+        self.expenses_table.setVerticalHeaderLabels(["День ", "Неделя ", "Месяц "])
         for i in range(2):
-            table.horizontalHeader().setSectionResizeMode(i, QHeaderView.Stretch if i == 1 else QHeaderView.ResizeToContents)
-        table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        set_data(table, [0, 0, 0], 1)
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Бюджет"))
-        layout.addWidget(table)
+            self.expenses_table.horizontalHeader().setSectionResizeMode(i, QtWidgets.QHeaderView.Stretch if i == 1 else QtWidgets.QHeaderView.ResizeToContents)
+        budget = Budget()
+        for i,l in zip(range(3), [BudgetDayItem, BudgetWeekItem, BudgetMonthItem]):
+            self.expenses_table.setItem(i, 1, l(budget))
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(QtWidgets.QLabel("Бюджет"))
+        layout.addWidget(self.expenses_table)
         self.setLayout(layout)
+        self.expense_presenter = expense_presenter
+        for i in range(3):
+            lost_item = QtWidgets.QTableWidgetItem()
+            lost_item.setFlags(lost_item.flags() & ~QtCore.Qt.ItemIsEditable)
+            self.expenses_table.setItem(i, 0, lost_item)
+        self.sign = self.expenses_table.itemChanged
+        self.sign.connect(self.edit_budget_event)
+        self.presenter = BudgetPresenter(self)
+        expenses = self.expense_getter()
+        budget = self.budget_getter()
+        self.update_expenses(expenses)
+        self.update_budget(budget)
