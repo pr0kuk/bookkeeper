245fb739318c6ef16c25a3239c27603dd5636f44
pr0kuk <alex-shch@yandex.ru>
2024-04-03 16:53:14 +0300

    used tree for categories

---
+++ bookkeeper/view/category_window.py
@@ -0,0 +1,22 @@
+"""
+Category Window
+"""
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QWidget
+
+class EditcategoryWindow(QWidget):
+    def __init__(self, parent) -> None:
+        super().__init__()
+        self.parent = parent
+        self.setWindowTitle("Изменение категорий")
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(QtWidgets.QLabel("Категории"))
+        layout.addWidget(self.parent.category_view)
+        cat_edit_button = QtWidgets.QPushButton("Выбрать")
+        cat_edit_button.clicked.connect(self.button_clicked)
+        layout.addWidget(cat_edit_button)
+        self.setLayout(layout)
+
+    def button_clicked(self):
+        self.parent.table.add_exp_event()
+        self.parent.table.close_category_window()
