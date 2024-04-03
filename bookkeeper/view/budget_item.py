15130232d805ba8b2ba03ee1f53ce0c154b978a9
pr0kuk <alex-shch@yandex.ru>
2024-04-03 18:55:07 +0300

    added budget items

---
+++ bookkeeper/view/budget_item.py
@@ -0,0 +1,39 @@
+from PySide6.QtWidgets import QTableWidgetItem
+from bookkeeper.models.budget import Budget
+
+class BudgetItem(QTableWidgetItem):
+    def __init__(self, budget: Budget):
+        super().__init__()
+        self.update(budget)
+    def get(self) -> Budget:
+        return self.budget
+
+class BudgetDayItem(BudgetItem):
+    def get_value(self) -> None | float:
+        try:
+            return float(self.text())
+        except ValueError:
+            return None
+    def update(self, budget: Budget) -> None:
+        self.budget = budget
+        self.setText(str(round(self.budget.amount, 2)))
+
+class BudgetWeekItem(BudgetItem):
+    def get_value(self) -> None | float:
+        try:
+            return float(self.text()) / 7
+        except ValueError:
+            return None
+    def update(self, budget: Budget) -> None:
+        self.budget = budget
+        self.setText(str(round(self.budget.amount * 7, 2)))
+
+class BudgetMonthItem(BudgetItem):
+    def get_value(self) -> None | float:
+        try:
+            return float(self.text()) / 30
+        except ValueError:
+            return None
+    def update(self, budget: Budget) -> None:
+        self.budget = budget
+        self.setText(str(round(self.budget.amount * 30, 2)))
