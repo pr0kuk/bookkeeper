9708a7f98e3dd69252c0c9785f8024963e2b6b46
pr0kuk <alex-shch@yandex.ru>
2024-04-03 19:55:25 +0300

    added category presenter

--- bookkeeper/view/presenters.py
+++ bookkeeper/view/presenters.py
@@ -31,3 +31,45 @@ class BudgetPresenter:
             self.repo.add(budget)
             budgets.append(budget)
         return budgets.pop()
+
+class CategoryPresenter:
+    def __init__(self,  view: CategoryConcept):
+        self.view = view
+        self.category_repo = get_rep(Category)
+        self.categories = self.category_repo.get_all()
+        self.view.set_category_list(self.categories)
+        self.view.register_category_modifier(self.modify_category)
+        self.view.register_category_adder(self.add_category)
+        self.view.register_category_checker(self.check_name)
+        self.view.register_category_deleter(self.delete_category)
+        self.view.register_category_finder(self.find_category_by_name)
+
+    def modify_category(self, category: Category) -> None:
+        self.category_repo.update(category)
+
+    def check_name(self, name: str) -> bool:
+        if name in [category.name for category in self.categories]:
+            return False
+        return True
+
+    def find_category_by_name(self, name: str) -> int | None:
+        for category in self.categories:
+            if category.name == name:
+                return int(category.pk)
+        return None
+
+    def add_category(self, category: Category) -> None:
+        self.category_repo.add(category)
+        self.categories.append(category)
+
+    def delete_category(self, top_lvl_category: Category) -> None:
+        queue = [top_lvl_category]
+        to_delete = []
+        while len(queue) != 0:
+            proc = queue.pop()
+            to_delete.append(proc)
+            queue.extend([x for x in self.categories if x.parent == proc.pk])
+        for x in to_delete:
+            self.categories.remove(x)
+            self.category_repo.delete(x.pk)
+
