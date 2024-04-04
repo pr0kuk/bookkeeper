f09a9bb57cf69a39196c77aa459b7755f1534e12
pr0kuk <alex-shch@yandex.ru>
2024-04-04 15:13:14 +0300

    added expense table

---
+++ bookkeeper/view/expense_table.py
@@ -0,0 +1,19 @@
+from .expense_items import ExpenseAmountItem, ExpenseCategoryItem, ExpenseItem, ExpenseRow, ExpenseDateItem
+from typing import Any
+from PySide6.QtWidgets import QTableWidget, QMenu, QMessageBox, QHeaderView
+from bookkeeper.models.expense import Expense
+
+class Table(QTableWidget):
+    def __init__(self, parent: Any):
+        super().__init__()
+        self.parent = parent
+        self.setColumnCount(4)
+        self.setRowCount(0)
+        self.setHorizontalHeaderLabels(["Дата ", "Сумма ", "Категория ", "Комментарий"])
+        for i in range(4):
+            self.horizontalHeader().setSectionResizeMode(i, QHeaderView.Stretch if i == 3 else QHeaderView.ResizeToContents)
+        self.menu = QMenu(self)
+        self.menu.addAction('Добавить').triggered.connect(self.open_category_window)
+        self.menu.addAction('Удалить').triggered.connect(self.delete_exp_event)
+        self.sign = self.itemChanged
+        self.sign.connect(self.update_exp_event)
