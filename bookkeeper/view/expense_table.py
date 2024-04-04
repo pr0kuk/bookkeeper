98e3bfca55628245c4b5104fceaac5bb7f356529
pr0kuk <alex-shch@yandex.ru>
2024-04-04 15:58:00 +0300

    added add/delete method

--- bookkeeper/view/expense_table.py
+++ bookkeeper/view/expense_table.py
@@ -24,3 +24,35 @@ class Table(QTableWidget):
     def close_category_window(self):
         self.parent.edit_category_window.close()
     
+    def add_expense(self, expense: Expense) -> None:
+        row = ExpenseRow(expense)
+        category_item = ExpenseCategoryItem(row, self.parent)
+        rcount = self.rowCount()
+        self.setRowCount(rcount+1)
+        self.sign.disconnect()
+        self.setItem(rcount, 0, ExpenseDateItem(row))
+        self.setItem(rcount, 1, ExpenseAmountItem(row))
+        self.setItem(rcount, 2, category_item)
+        self.setItem(rcount, 3, ExpenseItem(row))
+        self.sign.connect(self.update_exp_event)
+
+    def delete_exp_event(self) -> None:
+        row = self.currentRow()
+        if row == -1:
+            return
+        titem = self.item(row, 0)
+        self.removeRow(row)
+        assert isinstance(titem, ExpenseItem)
+        exp_to_del = titem.trow.expense
+        self.parent.exp_deleter(exp_to_del)
+        self.parent.emit_exp_changed()
+
+    def add_exp_event(self) -> None:
+        expense = Expense()
+        try:
+            self.add_expense(expense)
+        except ValueError as valerr:
+            QMessageBox.critical(self, 'Ошибка', f'{valerr}')
+            return
+        self.parent.exp_adder(expense)
+        self.parent.emit_exp_changed()
